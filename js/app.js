16c4b11c15bb6f056696cf6c466bb9f6bd319149
apudo-Sebby <sebbyapudo@gmail.com>
2022-06-28 03:39:25 +0300

    Implemented the js to show the scrollup button

--- js/app.js
+++ js/app.js
@@ -67,3 +67,38 @@ let newSwiper = new Swiper(".new-swiper", {
         },
     },
 });
+
+// SCROLL SECTIONS ACTIVE LINKS
+const sections = document.querySelectorAll('section[id]');
+
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+              if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+            }
+            else {
+                document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// SHOW SCROLL UP
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 400 viewport height, and the show-scroll tag to the a tag with the scroll-t
+    if (this.scrollY >= 400) {
+        scrollUp.classList.add('show-scroll')
+    }
+     else {
+         scrollUp.classList.remove('show-scroll')
+     }
+}
+window.addEventListener('scroll', scrollUp)
